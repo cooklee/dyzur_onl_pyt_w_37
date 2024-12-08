3aa2a02dd46a40b541c4d217493a699e32afbd04
cooklee <sbbogus@gmail.com>
2024-12-08 10:39:14 +0100

    test cdn

--- zmiana/conftest.py
+++ zmiana/conftest.py
@@ -26,3 +26,17 @@ def shifts(user, user2):
             sh.append(s)
         shifts[u.id] = sh
     return shifts
+
+
+@pytest.fixture
+def shifts_2(user):
+    shifts = {}
+    skip = 0
+    for active in [True, False]:
+        sh = []
+        for x in range(5):
+            s = Shift.objects.create(owner=user, active=active, date= datetime(1999,1,x+1+skip).date())
+            sh.append(s)
+        skip += 5
+        shifts[active] = sh
+    return shifts
--- zmiana/tests.py
+++ zmiana/tests.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.test import Client
 from django.urls import reverse
 
+from zmiana.models import Shift
+
 
 @pytest.mark.django_db
 def test_index_view():
@@ -32,5 +34,43 @@ def test_my_duty_login(user, shifts):
     assert user_shift.count() == len(shifts[user.id])
     for my_shift in shifts[user.id]:
         assert my_shift in user_shift
+
+
+@pytest.mark.django_db
+def test_my_duty_login_active(user, shifts_2):
+    client = Client()
+    client.force_login(user)
+    url = reverse('my_duty')  # pobieram z url'a adres na który bede chciał wejść
+    response = client.get(url)  # używam przeglądarki by wejść na zadany adres
+    assert response.status_code == 200
+    user_shift = response.context['shifts']
+    assert user_shift.count() == len(shifts_2[True])
+    for my_shift in shifts_2[True]:
+        assert my_shift in user_shift
+
+
+@pytest.mark.django_db
+def test_add_new_shift_get(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('add_new_shift')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_new_shift_post(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('add_new_shift')
+    dane = {
+        'date':'1999-09-09',
+    }
+    response = client.post(url, dane)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('add_new_shift'))
+    assert Shift.objects.count()==1
+
+
 
 
